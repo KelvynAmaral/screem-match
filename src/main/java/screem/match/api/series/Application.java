d57806a9affb08384eccf73627fe90b40b341b79
Kelvyn <dev.kelvynjohnson@gmail.com>
2023-11-11 07:49:45 -0300

    Ponto de partida

---
+++ src/main/java/screem/match/api/series/Application.java
@@ -0,0 +1,13 @@
+package screem.match.api.series;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/screem/match/api/series/ApplicationTests.java
@@ -0,0 +1,13 @@
+package screem.match.api.series;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
